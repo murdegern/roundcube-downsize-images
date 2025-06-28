acca90ba92238a765b550f4623b06b5e28762a7a
Torsten Luettgert <torsten.luettgert@thinkproject.com>
2025-06-28 18:16:13 +0200

    Implemented

--- downsize_images.php
+++ downsize_images.php
@@ -10,17 +10,44 @@ class downsize_images extends rcube_plugin
         $this->add_hook('attachment_upload', [$this, 'resize_attachment']);
     }
 
+    static public function resize_img($img) {
+      $size_x = imagesx($img);
+      $size_y = imagesy($img);
+      $factor = 1.0;
+      if ($size_x >= $size_y && $size_x > 1600) {
+        $factor = 1600.0 / $size_x;
+      } elseif ($size_y >= $size_x && $size_y > 1600) {
+        $factor = 1600.0 / $size_y;
+      }
+      rcube::write_log('downsize.log', "resize_jpeg: size=$size_x x $size_y; factor = $factor");
+
+      $new_img = imagescale($img, (int)(0.5+$size_x*$factor), -1, IMG_BICUBIC_FIXED);
+
+      return $new_img;
+    }
+
     static public function resize_attachment($args) {
         $rcmail = rcube::get_instance();
         $file = $args['path'];
         $size = filesize($file);
         $mime = strtolower($args['mimetype']);
         rcube::write_log('downsize.log', "resize_attachment($args) called; file=($file); size=($size); mime=($mime)");
-        if ($size < 1000000 || $mime ~= 'image/jpeg') {
+        if ($size < 1000000 || ($mime != 'image/jpeg' && $mime != 'image/png')) {
             return $args;
         }
         rcube::write_log('downsize.log', "would downsize this");
-        // TODO
+
+        if ($mime == 'image/jpeg') {
+          $img = imagecreatefromjpeg($filename);
+          $img = resize_image($img);
+          $success = imagejpeg($img, $filename, -1);
+        } elseif ($mime == 'image/png') {
+          $img = imagecreatefrompng($filename);
+          $img = resize_image($img);
+          $success = imagepng($img, $filename, 9);
+        }
+        rcube::write_log('downsize.log', "re-wrote $filename; success = $success");
+
         return $args;
     }
 }
